8ad2b723987f55037af72df1a97a59e9beaa9c0c
zhang shade <shadekassimi@gmail.com>
2022-11-23 19:06:30 +0800

    Initial Commit

---
+++ website/views.py
@@ -0,0 +1,213 @@
+from django.shortcuts import render
+
+def home(request):
+	return render(request,'home.html', {})
+
+def vivitara(request):
+	return render(request,'vivi-tarantino.html', {})
+
+def kacy(request):
+	return render(request,'kacy-black.html', {})
+
+def bum(request):
+	return render(request,'bella-bumzy.html', {})
+
+def sam(request):
+	return render(request,'sam-slayres.html', {})				
+
+def lana(request):
+	return render(request,'lana-rhoades.html', {})
+
+def bhad(request):
+	return render(request,'bhad-babie.html', {})
+
+def mia(request):
+	return render(request,'mia-khalifa.html', {})		
+
+def pia(request):
+	return render(request,'pia-mia.html', {})
+
+def meli(request):
+	return render(request,'melisa-belle.html', {})
+
+def stormy(request):
+	return render(request,'baby-girl-stormy.html', {})
+
+def malk(request):
+	return render(request,'mia-malkova.html', {})
+
+def dee(request):
+	return render(request,'sophie-dee.html', {})
+
+def rx(request):
+	return render(request,'karma-rx.html', {})
+
+def woods(request):
+	return render(request,'jordyn-woods.html', {})
+
+def reid(request):
+	return render(request,'riley-reid.html', {})
+
+def rose(request):
+	return render(request,'amber-rose.html', {})
+
+def lexi(request):
+	return render(request,'lexi-belle.html', {})
+
+def jade(request):
+	return render(request,'katrina-jade.html', {})
+
+def katie(request):
+	return render(request,'miss-katie.html', {})
+
+def rage(request):
+	return render(request,'rage-queen.html', {})
+
+def sonja(request):
+	return render(request,'sonja-morgan.html', {})
+
+def ch(request):
+	return render(request,'blac-chyna.html', {})
+
+def jm(request):
+	return render(request,'jem-wolfie.html', {})
+
+def nat(request):
+	return render(request,'natalie-monroe.html', {})	
+
+def bel(request):
+	return render(request,'belle-delphine.html', {})
+
+def th(request):
+	return render(request,'bella-thorne.html', {})
+
+def ana(request):
+	return render(request,'tana-mongeau.html', {})
+
+def ka(request):
+	return render(request,'kanricos.html', {})
+
+def megan(request):
+	return render(request,'megan-barton-hanson.html', {})
+
+def kopf(request):
+	return render(request,'corinna-kopf.html', {})
+
+def mon(request):
+	return render(request,'monica-huldt.html', {})
+
+def esk(request):
+	return render(request,'molly-eskam.html', {})
+
+def kate(request):
+	return render(request,'katelyn-runck.html', {})
+
+def sa(request):
+	return render(request,'sam-hickelspoon.html', {})
+
+def brooks(request):
+	return render(request,'haley-brooks.html', {})
+
+def daisy(request):
+	return render(request,'daisy-drew.html', {})
+
+def dana(request):
+	return render(request,'dana-dearmond.html', {})
+
+def shady(request):
+	return render(request,'shady-doll.html', {})
+
+def tisan(request):
+	return render(request,'lucy-tisan.html', {})
+
+def paul(request):
+	return render(request,'lena-paul.html', {})
+
+def adriana(request):
+	return render(request,'adriana-chechik.html', {})
+
+def noi(request):
+	return render(request,'kira-noir.html', {})
+
+def ho(request):
+	return render(request,'honey-asian.html', {})
+
+def pag(request):
+	return render(request,'paige-steele.html', {})
+
+def blu(request):
+	return render(request,'jewelz-blu.html', {})
+
+def ki(request):
+	return render(request,'miss-kirroyal.html', {})
+
+def ai(request):
+	return render(request,'aisha.html', {})
+
+def mi(request):
+	return render(request,'milkimind.html', {})
+
+def vip(request):
+	return render(request,'sasha-asian-vip.html', {})
+
+def bitt(request):
+	return render(request,'ittybittyprettykitty.html', {})
+
+def kat(request):
+	return render(request,'thekatykat.html', {})
+
+def sss(request):
+	return render(request,'samlypuff.html', {})
+
+def poke(request):
+	return render(request,'pokebella.html', {})
+
+def nas(request):
+	return render(request,'nastyaa.html', {})
+
+def jojo(request):
+	return render(request,'jojolovesex.html', {})	
+
+def jo(request):
+	return render(request,'lenina-crowne.html', {})
+
+def lily(request):
+	return render(request,'lilybigboobs.html', {})					
+
+def polly(request):
+	return render(request,'polly-kendall.html', {})
+
+def gem(request):
+	return render(request,'gemma-massey.html', {})
+
+def may(request):
+	return render(request,'victoria-may.html', {})	
+
+def bee(request):
+	return render(request,'kira-bee.html', {})
+
+def eze(request):
+	return render(request,'lauren-elizabeth.html', {})
+
+def iny(request):
+	return render(request,'tiny-vanessa.html', {})
+
+def cd(request):
+	return render(request,'alyssa-macdonald.html', {})
+
+def lyrics(request):
+	return render(request,'lyricsbylyla.html', {})
+
+def pro(request):
+	return render(request,'profiles.html', {})		
+
+
+
+
+
+
+			
+
+
+ 
+ 
